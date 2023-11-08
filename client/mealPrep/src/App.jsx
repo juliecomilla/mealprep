aeda958c568ec41b5e7b943ff52917571be40c5d
Kerem Deen <kerem.deen@gmail.com>
2023-11-08 10:50:03 -0500

    db fill

--- client/mealPrep/src/App.jsx
+++ client/mealPrep/src/App.jsx
@@ -1,4 +1,8 @@
 import { useState } from 'react'
+import {Routes,  Route } from 'react-router-dom'
+import Food from './component/Food'
+import Login from './component/Login'
+import Meal from './component/Meal'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,28 +11,21 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Routes>
+        <Route path="/login"
+          element={<Login/>}
+        />
+        
+        <Route path="/food"
+          element={<Food/>}
+        />
+        
+        <Route path="/meal"
+          element={<Meal/>}
+        />
+      </Routes>
+    </div>
   )
 }
 
