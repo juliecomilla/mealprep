d86d085e710cbc88d5cd74b5d37d0e0dbe71922b
Kerem Deen <kerem.deen@gmail.com>
2023-11-08 10:50:45 -0500

    db fill

--- server/app.py
+++ server/app.py
@@ -2,8 +2,9 @@
 
 from models import db, User, Meal, Cocktail, FavoriteMeal, FavoriteCocktail, MealReview, CocktailReview
 from flask_migrate import Migrate
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, session
 from flask_restful import Api, Resource
+
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -13,7 +14,7 @@ DATABASE = os.environ.get(
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+app.secret_key = "bad_apple"
 
 migrate = Migrate(app, db)
 
@@ -21,42 +22,168 @@ db.init_app(app)
 
 @app.route('/')
 def home():
-    return '<h1>Code challenge</h1>'
+    return '<h1>Phase 4 Group Project</h1>'
 
-@app.route('/users')
-def users():
-    body = [user.to_dict() for user in User.query.all]
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+    try:
+        new_user = User(username=data['username'])
+        new_user.password_hash = data['password']
+    except ValueError as e:
+        return {'error':str(e)}, 400
+    db.session.add(new_user)
+    db.session.commit()
+
+    return {'message': f'{new_user.username} added'}, 201
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    user = User.query.filter(User.username == data['username']).first()
+
+    if not user:
+        return {'message': 'user not found'}, 404
+    
+    if user.authenticate(data['password']):
+        session['user_id'] = user.id
+        return {'message': 'login success'}, 201
+    else:
+        return {'message': 'login failed'}, 401
+    
+@app.route('/logout', methods=['DELETE'])
+def logout():
+    session.pop('user_id')
+    return {'message':"logged out"}, 200
+
+@app.route('/meals', methods=['GET'])
+def all_meals():
+    body = [meal.to_dict() for meal in Meal.query.all()]
     return body, 200
 
-@app.route('/meals')
-def meals():
-    body = [meal.to_dict() for meal in Meal.query.all]
+@app.route('/cocktails', methods=['GET'])
+def all_cocktails():
+    body = [user.to_dict() for user in Cocktail.query.all()]
     return body, 200
 
-@app.route('/cocktails')
-def cocktails():
-    body = [user.to_dict() for user in Cocktail.query.all]
-    return body, 200
+@app.route('/favmeals', methods=['POST'])
+def all_favmeals():
+    data = request.get_json()
 
-@app.route('/favmeals')
-def favmeals():
-    body = [user.to_dict() for user in FavoriteMeal.query.all]
-    return body, 200
+    try:
+        new_fav = FavoriteMeal(user_id = data.get('user_id'), meal_id = data.get('meal_id'))
+    except ValueError as e:
+        return {'message':str(e)}, 400
+    
+    db.session.add(new_fav)
+    db.session.commit()
 
-@app.route('/favcocks')
-def favcocks():
-    body = [user.to_dict() for user in FavoriteCocktail.query.all]
-    return body, 200
+    return new_fav.to_dict(), 201
 
-@app.route('/mealrevs')
-def mealrevs():
-    body = [user.to_dict() for user in MealReview.query.all]
-    return body, 200
+@app.route('/favmeals/<int:id>', methods=['GET','DELETE'])
+def favmeal_by_id(id):
+    body = FavoriteMeal.query.filter(FavoriteMeal.id == id).first()
 
-@app.route('/cockrevs')
-def cockrevs():
-    body = [user.to_dict() for user in CocktailReview.query.all]
-    return body, 200
+    if not body:
+        return {'message':'favorite meal not found'}, 404
+    if request.method == 'GET':
+        return body.to_dict(), 200
+    elif request.method == 'DELETE':
+        db.session.delete(body)
+        db.session.commit()
+        return {}, 204
+        
+
+@app.route('/favcocks', methods=['POST'])
+def all_favcocks():
+    data = request.get_json()
+
+    try:
+        new_fav = FavoriteCocktail(user_id = data.get('user_id'), cocktail_id = data.get('meal_id'))
+    except ValueError as e:
+        return {'message':str(e)}, 400
+    
+    db.session.add(new_fav)
+    db.session.commit()
+
+    return new_fav.to_dict(), 201
+
+@app.route('/favcocks/<int:id>', methods=['GET','DELETE'])
+def favcock_by_id(id):
+    body = FavoriteCocktail.query.filter(FavoriteCocktail.id == id).first()
+
+    if not body:
+        return {'message':'favorite cocktail not found'}, 404
+    if request.method == 'GET':
+        return body.to_dict(), 200
+    elif request.method == 'DELETE':
+        db.session.delete(body)
+        db.session.commit()
+        return {}, 204
+
+@app.route('/mealrevs', methods=['POST'])
+def all_mealrevs():
+    data = request.get_json()
+
+    try:
+        new_rev = MealReview(user_id = data.get('user_id'), meal_id = data.get('meal_id'), comment = data.get('comment'))
+    except ValueError as e:
+        return {'message':str(e)}, 400
+    
+    db.session.add(new_rev)
+    db.session.commit()
+
+    return new_rev.to_dict(), 201
+
+@app.route('/mealrevs/<int:id>', methods=['GET','PATCH'])
+def mealrev_by_id(id):
+    body = MealReview.query.filter(MealReview.id == id).first()
+
+    if not body:
+        return {'message':'meal review not found'}, 404
+    if request.method == 'GET':
+        return body.to_dict(), 200
+    elif request.method == 'PATCH':
+        data = request.get_json()
+
+        for field in data:
+            setattr(body, field, data[field])
+        
+        db.session.add(body)
+        db.session.commit()
+        return {}, 200
+
+@app.route('/cockrevs', methods=['POST'])
+def all_cockrevs():
+    data = request.get_json()
+    
+    try:
+        new_rev = CocktailReview(user_id = data.get('user_id'), cocktail_id = data.get('cocktail_id'), comment = data.get('comment'))
+    except ValueError as e:
+        return {'message':str(e)}, 400
+    
+    db.session.add(new_rev)
+    db.session.commit()
+
+    return new_rev.to_dict(), 201
+
+@app.route('/mealrevs/<int:id>', methods=['GET','PATCH'])
+def cockrev_by_id(id):
+    body = CocktailReview.query.filter(CocktailReview.id == id).first()
+
+    if not body:
+        return {'message':'cocktail review not found'}, 404
+    if request.method == 'GET':
+        return body.to_dict(), 200
+    elif request.method == 'PATCH':
+        data = request.get_json()
+
+        for field in data:
+            setattr(body, field, data[field])
+        
+        db.session.add(body)
+        db.session.commit()
+        return {}, 200
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/seed.py
+++ server/seed.py
@@ -2,9 +2,11 @@ from random import choice as rc, randrange
 
 from app import app
 from models import db, User, MealReview, Meal, FavoriteCocktail, FavoriteMeal, Cocktail, CocktailReview
+import requests
 
 if __name__ == '__main__':
     with app.app_context():
+
         print('Clearing DB...')
         User.query.delete()
         Meal.query.delete()
@@ -14,78 +16,128 @@ if __name__ == '__main__':
         Cocktail.query.delete()
         CocktailReview.query.delete()
 
-        print('Seeding Users')
-        users = [
-            User(username="Marge"),
-            User(username="John"),
-            User(username="Lucas"),
-            User(username="Dom")
-        ]
+        print('Seeding meals')
+        mealdata = requests.get('http://127.0.0.1:3000/meals')
+        new_data = mealdata.json()
+        new_meals = []
+        for meal in new_data:
+            new_meals.append(
+                Meal(
+                    name = meal.get('strMeal'),
+                    base = meal.get('strCategory'),
+                    origin = meal.get('strArea'),
+                    instruction = meal.get('strInstructions'),
+                    image = meal.get('strMealThumb'),
+                    ingred1 = meal.get('strIngredient1'),
+                    ingred2 = meal.get('strIngredient2'),
+                    ingred3 = meal.get('strIngredient3'),
+                    ingred4 = meal.get('strIngredient4'),
+                    ingred5 = meal.get('strIngredient5'),
+                    ingred6 = meal.get('strIngredient6'),
+                    ingred7 = meal.get('strIngredient7'),
+                    ingred8 = meal.get('strIngredient8'),
+                    ingred9 = meal.get('strIngredient9'),
+                    ingred10 = meal.get('strIngredient10'),
+                    ingred11 = meal.get('strIngredient11'),
+                    ingred12 = meal.get('strIngredient12'),
+                    ingred13 = meal.get('strIngredient13'),
+                    ingred14 = meal.get('strIngredient14'),
+                    ingred15 = meal.get('strIngredient15'),
+                    ingred16 = meal.get('strIngredient16'),
+                    ingred17 = meal.get('strIngredient17'),
+                    ingred18 = meal.get('strIngredient18'),
+                    ingred19 = meal.get('strIngredient19'),
+                    measure1 = meal.get('strMeasure1'),
+                    measure2 = meal.get('strMeasure2'),
+                    measure3 = meal.get('strMeasure3'),
+                    measure4 = meal.get('strMeasure4'),
+                    measure5 = meal.get('strMeasure5'),
+                    measure6 = meal.get('strMeasure6'),
+                    measure7 = meal.get('strMeasure7'),
+                    measure8 = meal.get('strMeasure8'),
+                    measure9 = meal.get('strMeasure9'),
+                    measure10 = meal.get('strMeasure10'),
+                    measure11 = meal.get('strMeasure11'),
+                    measure12 = meal.get('strMeasure12'),
+                    measure13 = meal.get('strMeasure13'),
+                    measure14 = meal.get('strMeasure14'),
+                    measure15 = meal.get('strMeasure15'),
+                    measure16 = meal.get('strMeasure16'),
+                    measure17 = meal.get('strMeasure17'),
+                    measure18 = meal.get('strMeasure18'),
+                    measure19 = meal.get('strMeasure19')
+            ))
+        db.session.add_all(new_meals)
 
-        for user in users:
-            user.password_hash = 'abc'
+        print('Seeding cocktails')
+        cocktaildata = requests.get('http://127.0.0.1:3000/cocktails')
+        new_data = cocktaildata.json()
+        new_cocktails = []
+        for cocktail in new_data:
+            new_cocktails.append(
+                Cocktail(
+                    name = cocktail.get('strDrink'),
+                    alcoholic = cocktail.get('strAlcoholic'),
+                    instruction = cocktail.get('strInstructions'),
+                    image = cocktail.get('strDrinkThumb'),
+                    ingred1 = cocktail.get('strIngredient1'),
+                    ingred2 = cocktail.get('strIngredient2'),
+                    ingred3 = cocktail.get('strIngredient3'),
+                    ingred4 = cocktail.get('strIngredient4'),
+                    ingred5 = cocktail.get('strIngredient5'),
+                    measure1 = cocktail.get('strMeasure1'),
+                    measure2 = cocktail.get('strMeasure2'),
+                    measure3 = cocktail.get('strMeasure3'),
+                    measure4 = cocktail.get('strMeasure4'),
+                    measure5 = cocktail.get('strMeasure5'),
+            ))
+        db.session.add_all(new_cocktails)
 
-        db.session.add_all(users)
-
-        print('Seeding Meals')
-        meals = [
-            Meal(name="Spaget",origin="Ita",base="Beef", instruction="Gravy"),
-            Meal(name="Grill Cheese",origin="USA",base="Cheese", instruction="Grill"),
-            Meal(name="Halal",origin="Egy",base="Chicken", instruction="Burn"),
-            Meal(name="Sandwich",origin="CDN",base="Bread", instruction="Fire")
-        ]
-
-        db.session.add_all(meals)
-
-        print('Seeding Cocktails')
-        cocktails = [
-            Cocktail(name="Ovaltine", base="Milk", instruction="Pull"),
-            Cocktail(name="GamerS", base="Powder", instruction="Mix"),
-            Cocktail(name="Magarita", base="Tequila", instruction="Shake"),
-            Cocktail(name="Water", base="Water", instruction="Nothing")
-        ]
-
-        db.session.add_all(cocktails)
-
-        print('Seeding Favorite Meals')
-        fav_meals = [
-            FavoriteMeal(user_id=1, meal_id=2),
-            FavoriteMeal(user_id=1, meal_id=3),
-            FavoriteMeal(user_id=2, meal_id=2),
-            FavoriteMeal(user_id=2, meal_id=1)
-        ]
-
-        db.session.add_all(fav_meals)
-
-        print('Seeding Favorite Cocktails')
-        fav_cocktails = [
-            FavoriteCocktail(user_id=3, cocktail_id=2),
-            FavoriteCocktail(user_id=3, cocktail_id=4),
-            FavoriteCocktail(user_id=4, cocktail_id=2),
-            FavoriteCocktail(user_id=4, cocktail_id=3)
-        ]
-
-        db.session.add_all(fav_cocktails)
-
-        print('Seeding Meal Reviews')
-        meal_rev = [
-            MealReview(user_id=1, meal_id=1, comment="mid"),
-            MealReview(user_id=1, meal_id=3, comment="top"),
-            MealReview(user_id=2, meal_id=2, comment="bottom"),
-            MealReview(user_id=2, meal_id=3, comment="best")
-        ]
-
-        db.session.add_all(meal_rev)
-
-        print('Seeding Cocktail Reviews')
-        cock_rev = [
-            CocktailReview(user_id=3, cocktail_id=1, comment="bottom"),
-            CocktailReview(user_id=3, cocktail_id=4, comment="top"),
-            CocktailReview(user_id=4, cocktail_id=4, comment="best"),
-            CocktailReview(user_id=4, cocktail_id=3, comment="mid")
-        ]
-
-        db.session.add_all(cock_rev)
+        print('adding to DB...')
         db.session.commit()
 
-        print('Finish Seeding!')
+        print('Done seeding')
+        # print('Clearing DB...')
+        # User.query.delete()
+        # Meal.query.delete()
+        # MealReview.query.delete()
+        # FavoriteCocktail.query.delete()
+        # FavoriteMeal.query.delete()
+        # Cocktail.query.delete()
+        # CocktailReview.query.delete()
+
+        # print('Seeding Users')
+        # users = [
+        #     User(username="Marge"),
+        #     User(username="John"),
+        #     User(username="Lucas"),
+        #     User(username="Dom")
+        # ]
+
+        # for user in users:
+        #     user.password_hash = 'abc'
+
+        # db.session.add_all(users)
+
+        # print('Seeding Meals')
+        
+        # meals = [
+        #     Meal(
+        #         name="Spaget",origin="Ita",base="Beef", instruction="Gravy"),
+            
+        # ]
+
+        # db.session.add_all(meals)
+
+        # print('Seeding Cocktails')
+        # cocktails = [
+        #     Cocktail(name="Ovaltine", base="Milk", instruction="Pull"),
+        #     Cocktail(name="GamerS", base="Powder", instruction="Mix"),
+        #     Cocktail(name="Magarita", base="Tequila", instruction="Shake"),
+        #     Cocktail(name="Water", base="Water", instruction="Nothing")
+        # ]
+
+        # db.session.add_all(cocktails)
+        # db.session.commit()
+
+        # print('Finish Seeding!')
